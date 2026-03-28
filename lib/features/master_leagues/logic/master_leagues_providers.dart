564edf2aad41c41c6c0021010f936a094609d933
Mohammed Abubakar <mohammedauwalnasara@gmail.com>
2026-03-29 00:45:26 +0100

    fix(master-leagues): restore providers and show all active organizers in discovery

--- lib/features/master_leagues/logic/master_leagues_providers.dart
+++ lib/features/master_leagues/logic/master_leagues_providers.dart
@@ -2,7 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../data/master_leagues_repository_firebase.dart';
 import '../data/organizer_feed_firebase.dart';
+import '../domain/competition_template.dart';
 import '../domain/master_league.dart';
+import '../domain/master_league_plan.dart';
 import 'master_league_entitlement_service.dart';
 import 'master_league_payment_service.dart';
 
@@ -25,9 +27,8 @@ final masterLeagueEntitlementServiceProvider =
   return MasterLeagueEntitlementService();
 });
 
-final myMasterLeaguesProvider = StreamProvider.autoDispose<List<MasterLeague>>((
-  ref,
-) {
+final myMasterLeaguesProvider =
+    StreamProvider.autoDispose<List<MasterLeague>>((ref) {
   final repo = ref.watch(masterLeaguesRepositoryProvider);
   return repo.watchMyMasterLeagues();
 });
@@ -42,6 +43,45 @@ final joinedMasterLeaguesProvider =
     StreamProvider.autoDispose<List<MasterLeague>>((ref) {
   final repo = ref.watch(masterLeaguesRepositoryProvider);
   return repo.watchJoinedMasterLeagues();
+});
+
+final masterLeagueByIdProvider =
+    FutureProvider.autoDispose.family<MasterLeague?, String>((ref, id) async {
+  final repo = ref.watch(masterLeaguesRepositoryProvider);
+  return repo.getById(id);
+});
+
+final masterLeagueFollowStateProvider =
+    StreamProvider.autoDispose.family<bool, String>((ref, masterLeagueId) {
+  final repo = ref.watch(masterLeaguesRepositoryProvider);
+  return repo.watchIsFollowing(masterLeagueId);
+});
+
+final masterLeagueFollowersCountProvider =
+    StreamProvider.autoDispose.family<int, String>((ref, masterLeagueId) {
+  final repo = ref.watch(masterLeaguesRepositoryProvider);
+  return repo.watchFollowersCount(masterLeagueId);
+});
+
+final masterLeagueCompetitionTemplatesProvider =
+    StreamProvider.autoDispose.family<List<CompetitionTemplate>, String>((
+  ref,
+  masterLeagueId,
+) {
+  final repo = ref.watch(masterLeaguesRepositoryProvider);
+  return repo.watchCompetitionTemplates(masterLeagueId);
+});
+
+final organizerProActivePlanProvider =
+    FutureProvider.autoDispose<MasterLeaguePlan?>((ref) async {
+  final entitlement = ref.watch(masterLeagueEntitlementServiceProvider);
+  return entitlement.getActivePlan(forceRefresh: false);
+});
+
+final masterLeagueUnlockedProvider =
+    FutureProvider.autoDispose<bool>((ref) async {
+  final entitlement = ref.watch(masterLeagueEntitlementServiceProvider);
+  return entitlement.isUnlocked(forceRefresh: false);
 });
 
 final featuredOrganizerWorkspacesProvider =
