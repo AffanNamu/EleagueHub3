388a83cae51d002b72d025c9f939f047616f9870
MohammedAuwal <mohammedauwalnasara@gmail.com>
2026-07-22 01:03:07 +0100

    fix: restore missing imports and correct route configuration

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/package_core.dart' show FirebaseException;
+import 'package:firebase_core/firebase_core.dart' show FirebaseException;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
